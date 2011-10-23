e5af4748d8a087dc273d517e6e84d50089a295aa
cono <q@cono.org.ua>
2011-10-24 00:51:20 +0300

    * Add simple pod documentation

--- lib/MPQ.pm
+++ lib/MPQ.pm
@@ -1,5 +1,31 @@
 package MPQ;
 
+=head1 NAME
+
+MPQ - Perl module to parse MoPaQ Archive Format
+
+=head1 SYNOPSIS
+
+    use MPQ;
+
+    my $mpq = MPQ->new(filename => 'my_game.sc2replay');
+    $mpq->parse;
+
+    my $archive = $mpq->archive;
+    print for $archive->list_file;
+    print $archive->file('(listfile)')->slurp;
+
+=head1 DESCRIPTION
+
+This module give you a raw access to MPQ archive files.
+
+MPQ, is an archiving file format used in several of Blizzard Entertainment's
+games.
+
+=head1 METHODS
+
+=cut
+
 use strict;
 use warnings;
 
@@ -9,6 +35,14 @@ use MPQ::Archive;
 use MPQ::Shunt;
 
 our $VERSION = '0.01';
+
+=head2 new
+
+Constructor for the MPQ class. Has only one input parameter:
+
+    filename - path to the mpq archive file
+
+=cut
 
 sub new {
     my ($class, %param) = @_;
@@ -20,16 +54,22 @@ sub new {
     return bless(\%param, $class);
 }
 
+=head2 parse
+
+Method to do the parse of the MPQ file
+
+=cut
+
 sub parse {
     my $self = shift;
 
     $self->{'_file'}->open;
-    $self->do_magic;
+    $self->_do_magic;
 
-    $self->recognize_type;
+    $self->_recognize_type;
 }
 
-sub recognize_type {
+sub _recognize_type {
     my $self = shift;
     my $str = $self->{'_file'}->read_str(1);
 
@@ -56,20 +96,34 @@ sub recognize_type {
     $self->{'_archive'}->parse;
 }
 
-sub do_magic {
+sub _do_magic {
     my $self = shift;
     my $str = $self->{'_file'}->read_str(3);
 
     die "File is not a MPQ format (wrong magic)" unless $str eq MAGIC;
 }
 
+=head2 shunt
+
+Accessor to the L<MPQ::Shunt> object (could be without it)
+
+=cut
+
 sub shunt { $_[0]->{'_shunt'} }
+
+=head2 archive
+
+Accessor to the L<MPQ::Archive> object - heart of the MPQ file.
+
+=cut
 
 sub archive { $_[0]->{'_archive'} }
 
 1;
 
 =head1 AUTHOR
+
+cono C<q@cono.org.ua>
 
 C corporation (c)
 
